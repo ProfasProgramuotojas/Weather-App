40c7b1a2b95a3a88744f66113304dda52ea11d8a
ProfasProgramuotojas <mykolas.mickus@gmail.com>
2026-06-21 19:32:06 +0300

    suggestCities.ts optimisations + added population size cities.json

--- app/lib/suggestCities.ts
+++ app/lib/suggestCities.ts
@@ -1,15 +1,27 @@
 "use server";
 
-import cities from "../data/cities.json";
 import { CityType } from "@/app/types/CityType";
+import citiesData from "../data/cities.json";
+
+const cities = citiesData as CityType[];
 
 const LIMIT = 5;
 
+//top  5 most populated cities in the world
+const topCities = cities.slice(0, LIMIT);
+
 const suggestCities = async (query: string): Promise<CityType[]> => {
+  if (!query) return topCities;
+
+  //cities who start with query
   const autocompletedCities = cities.filter(
     (c) => c.name.slice(0, query.length) === query,
   );
 
+  if (autocompletedCities.length >= LIMIT)
+    return autocompletedCities.slice(0, LIMIT);
+
+  //cities who include query in them
   const searchedCities = cities
     .filter((c) => c.name.includes(query))
     .filter((c) => !autocompletedCities.some((ac) => ac.name === c.name));
--- app/types/CityType.ts
+++ app/types/CityType.ts
@@ -1,8 +1,10 @@
 export type CityType = {
+  id: number;
   name: string;
   ascii: string;
   country: string;
   lat: number;
   lon: number;
+  population: number;
   timezone: string;
 };
